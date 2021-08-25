4a06122d78a63b6db481e6e1ff511cdca3d081ac
muhaziramadhan <89523982+muhaziramadhan@users.noreply.github.com>
2021-08-25 20:36:47 +0800

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var about = document.getElementById ("about")
+    about.classList.toggle("dark-mode");
+ }
